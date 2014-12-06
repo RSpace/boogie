ba62a6f731b57fea1953f2fcc7ab6c5b00764e70
Casper Fabricius <me@casperfabricius.com>
2014-12-06 10:59:19 +0100

    Validate presence of user name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:username]
 
+  validates_presence_of :username
+
   def email_required?
     false
   end
