ee1eb7a84432902f151540699f873adbc4e118c2
Casper Fabricius <me@casperfabricius.com>
2015-10-19 20:25:39 +0200

    Added admin flag to user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,4 @@ class User < ActiveRecord::Base
   def email_required?
     false
   end
-
-  def admin?
-    # TODO: Allow for multiple admin users
-    self.username == 'admin'
-  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151019175212) do
+ActiveRecord::Schema.define(version: 20151019181955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,12 +28,12 @@ ActiveRecord::Schema.define(version: 20151019175212) do
   add_index "bookings", ["user_id"], name: "index_bookings_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "username",               default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "username",               default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20151019175212) do
     t.string   "phone"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin",                  default: false, null: false
   end
 
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
